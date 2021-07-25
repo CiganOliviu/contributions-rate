0e8358a874b84fc07555098eee550fd257499802
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-07-25 23:27:57 +0300

    Update Program.cs

--- ContributionsRate/ContributionsRate/Program.cs
+++ ContributionsRate/ContributionsRate/Program.cs
@@ -45,7 +45,7 @@ namespace ContributionsRate
     {
         private static void Main(string[] args)
         {
-            var rates = new Rates(2278);
+            var rates = new Rates(2334);
 
             Console.WriteLine(rates.GetAnnualContributionsRate());
             Console.WriteLine(rates.PredictNumberOfAnnualContributions());
